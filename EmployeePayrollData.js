95f6120f4898a1f35b5a208048119283131c2937
drisyaathul <drisyak809@gmail.com>
2023-06-15 10:42:52 +0800

    Added

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -5,39 +5,44 @@ class EmployeeParollData {
      */
 
     get name() {
-        return this.name;
+        return this._name;
     }
-    set name (name) {
-        let name = RegExp("^[A-Z]{1}[a-zA-z\\s]{2,}$");
-        if (name.test(name))
-        this.name = name;
-        else throw "Name is Incorrect";
+
+    set name( name ) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+        if ( nameRegex.test(name) )
+            this._name = name;
+        else throw "Name is incorrect";
     }
 
     get profilePic() {
-        return this.profilePic;
+        return this._profilePic;
     }
-    set profilePic (profilePic) {
-        this.profilePic = profilePic;
+
+    set profilePic ( profilePic ) {
+        this._profilePic = profilePic;
     }
 
     get gender() {
-        return this.gender;
+        return this._gender;
     }
-    set gender (gender) {
-        this.gender = gender;
+
+    set gender ( gender ) {
+        this._gender = gender;
     }
 
     get department() {
-        return this.department;
+        return this._department;
     }
-    set department(department) {
-        this.department = department;
+
+    set department ( department ) {
+        this._department = department;
     }
 
     get salary() {
         return this._salary;
     }
+
     set salary ( salary ) {
         this._salary = salary;
     }
@@ -45,6 +50,7 @@ class EmployeeParollData {
     get note() {
         return this._note;
     }
+
     set note ( note ) {
         this._note = note;
     }
@@ -52,16 +58,17 @@ class EmployeeParollData {
     get start_date() {
         return this._start_date;
     }
+
     set start_date ( start_date ) {
         this._start_date = start_date;
     }
 
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.start_date ? "undefined" : this.start_date.toLocalDataString("en-US", options);
+        const empDate = !this.start_date ? "undefined" :
+                        this.start_date.toLocaleDateString("en-US", options);
         return 'Name = ' + this.name + ", Gender = " + this.gender + ", ProfilePic = " +this.profilePic
-        + ", Department = " + this.department + ", Salary = " + this.salary +
-            ", StartDate = " + empDate + ", Note = " +this.note;
-    
+                    + ", Department = " + this.department + ", Salary = " + this.salary +
+                        ", StartDate = " + empDate + ", Note = " +this.note;
     }
 }
--- EmployeeQuery.js
+++ EmployeeQuery.js
@@ -1,25 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
     const name = document.querySelector('#name');
     const nameError = document.querySelector('.text-error');
-    name.addEventListener('input',function() {
-        if (name.value.length == 0) {
-            nameError.textContent = '';
-            return;
-        }try {
-            (new EmployeePayrollData()).name = name.value;
-            nameError.textContent = '';
-        }catch (e) {
-            nameError.textContent = e;
-        }
-    });
-    const startDate = document.querySelector('#start-date');
-    const dateError = document.querySelector('.date-error');
-    startDate.addEventListener('input', function() {
+    name.addEventListener('input', function() {
+      if (name.value.length == 0) {
+        nameError.textContent = '';
+        return;
+      }
       try {
-        (new EmployeePayrollData()).startDate = new Date(startDate.value);
-        dateError.textContent = '';
+        (new EmployeePayrollData()).name = name.value;
+        nameError.textContent = '';
       } catch (e) {
-        dateError.textContent = e;
+        nameError.textContent = e;
       }
     });
-})
+  });
+  
